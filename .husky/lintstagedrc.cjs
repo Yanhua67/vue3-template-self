9ce0a0ce68a8b30bf4558b824651d9c80a2b7915
yanhua67 <574207609@qq.com>
2024-08-14 19:38:38 +0800

    test

--- .husky/lintstagedrc.cjs
+++ .husky/lintstagedrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
   // 根据每一类文件分别进行校验
   '*.{js, jsx, ts, tsx}': ['eslint --fix', 'prettier --write'],
-  // '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-  //   'prettier --write--parser json'
-  // ],
-  // 'package.json': ['prettier --write'],
-  // '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
-  // '*.{scss, less, styl, html}': ['stylelint --fix', 'prettier --write'],
-  // '*.md': ['prettier --write']
+  '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
+    'prettier --write--parser json'
+  ],
+  'package.json': ['prettier --write'],
+  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  '*.{scss, less, styl, html}': ['stylelint --fix', 'prettier --write'],
+  '*.md': ['prettier --write']
 }
